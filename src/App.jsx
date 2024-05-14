85d8e0e1fae6420b401f15ddfd318712d65f4619
Jynxae <realkhalily@hotmail.com>
2024-05-14 18:42:26 -0500

    Created About me page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./navbar";
+import About from "./pages/About";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <About />
     </div>
   );
 }
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,27 +1,37 @@
 import React from "react";
+import myCV from "./Alkhalily_Reem.pdf"; // Import the PDF file
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800">
-      <ul className="flex">
-        <li className="mr-6">
-          <a href="#" className="text-white hover:text-gray-300">
-            Home
+    <nav className="bg-white-800">
+      <ul className="flex justify-end pr-60 pt-6 text-lg font-medium">
+        <li className="mr-6 relative">
+          <a href="#" className="text-black-600 group">
+            <span>About</span>
+            <span className="absolute bottom-0 left-0 bg-black h-0.5 w-full transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
           </a>
         </li>
-        <li className="mr-6">
-          <a href="#" className="text-white hover:text-gray-300">
-            About
+        <li className="mr-6 relative">
+          <a href="#" className="text-black-600 group">
+            <span>Projects</span>
+            <span className="absolute bottom-0 left-0 bg-black h-0.5 w-full transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
           </a>
         </li>
-        <li className="mr-6">
-          <a href="#" className="text-white hover:text-gray-300">
-            Services
+        <li className="mr-6 relative">
+          <a href="#" className="text-black-600 group">
+            <span>Contact</span>
+            <span className="absolute bottom-0 left-0 bg-black h-0.5 w-full transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
           </a>
         </li>
-        <li>
-          <a href="#" className="text-white hover:text-gray-300">
-            Contact
+        <li className="mr-6 relative">
+          <a
+            href={myCV}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black-600 group"
+          >
+            <span>Download my CV</span>
+            <span className="absolute bottom-0 left-0 bg-black h-0.5 w-full transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
           </a>
         </li>
       </ul>
