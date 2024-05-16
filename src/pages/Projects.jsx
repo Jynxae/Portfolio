641218098e024575ab5860ad829a29e09cd3959a
Jynxae <realkhalily@hotmail.com>
2024-05-16 18:04:52 -0500

    Fixed the formatting issue in projects.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
     <div className="flex flex-col items-center justify-center py-3 bg-white">
       <div className="bg-gray-200 rounded-3xl px-3 py-1 mt-3">Projects</div>
       <p className="mt-3">Here are some stuff I've worked on in the past</p>
-      <div className="grid grid-cols-2 bg-neutral-300 gap-12 mt-9 ml-12">
+      <div className="grid grid-cols-2 gap-24 mt-9 ml-72">
         <div>
           <img
             src={Museum}
@@ -14,7 +14,14 @@ function Projects() {
             className="w-full h-96 rightrounded-lg"
           />
         </div>
-        <div>description</div>
+        <div>
+          <h1 className="font-bold">The Baker Museum</h1>
+          <p className="mt-6">
+            This was a fun project I enjoyed working on with my friends. In this
+            project, we created a fullstack site using many tools such as Node,
+            mySQL workbench, Postman, Azure, Render, and Vercel.
+          </p>
+        </div>
         <div>skills</div>
       </div>
     </div>
