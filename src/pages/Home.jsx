b26e7250a44e18e04b41bab1a02e81d3397c16e4
Jynxae <realkhalily@hotmail.com>
2024-05-16 17:52:41 -0500

    Added a profile picture :D

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
+import profileImage from "../Images/pfp.jpg"; // Import your profile image
 
 function Home() {
   return (
-    <div className="pl-32">
-      <h1 className="flex text-7xl font-light pt-40">Hey, I'm Reem!</h1>
-      <p className="text-lg pt-9">
-        I'm a Software Engineer intern at Boeing starting Summer 2024! <br></br>
-        I'm entering my senior year at the University of Houston as a Computer
-        Science major<br></br> with a minor in Mathematics.
-      </p>
-      <div className=" flex pt-3 text-lg">
-        <FaLocationDot /> Houston, TX
+    <div className="pl-32 flex">
+      <div className="flex flex-col">
+        <h1 className="text-7xl font-light pt-40">Hey, I'm Reem!</h1>
+        <p className="text-lg pt-9">
+          I'm a Software Engineer intern at Boeing starting Summer 2024!{" "}
+          <br></br>
+          I'm entering my senior year at the University of Houston as a Computer
+          Science major<br></br> with a minor in Mathematics.
+        </p>
+        <div className="flex pt-3 text-lg">
+          <FaLocationDot /> Houston, TX
+        </div>
+      </div>
+      <div className="h-72 w-72 rounded-full overflow-hidden ml-36 mt-28">
+        <img
+          src={profileImage}
+          alt="This is me! Reem Alkhaliy"
+          className="object-cover h-full w-full"
+        />
       </div>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Museum from "../Images/Museum.jpg"; // Import your image file
 
 function Projects() {
   return (
-    <div className="flex justify-end mt-12 py-12 bg-white relative">
-      <div className="absolute top-3 left-2/4 bg-gray-200 rounded-3xl px-3 py-1 mt-3">
-        Projects
+    <div className="flex flex-col items-center justify-center py-3 bg-white">
+      <div className="bg-gray-200 rounded-3xl px-3 py-1 mt-3">Projects</div>
+      <p className="mt-3">Here are some stuff I've worked on in the past</p>
+      <div className="grid grid-cols-2 bg-neutral-300 gap-12 mt-9 ml-12">
+        <div>
+          <img
+            src={Museum}
+            alt="Databases Project for COSC 3380"
+            className="w-full h-96 rightrounded-lg"
+          />
+        </div>
+        <div>description</div>
+        <div>skills</div>
       </div>
     </div>
   );
