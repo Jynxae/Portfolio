109ccbe23056485983bf9f7ddf792941043172e9
Jynxae <realkhalily@hotmail.com>
2024-05-17 00:18:18 -0500

    FINALLY GOT THE GRID WORKING IN THE PROJECTS SECTION

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,8 +6,8 @@ function Projects() {
     <div className="flex flex-col items-center justify-center py-3 bg-white">
       <div className="bg-gray-200 rounded-3xl px-3 py-1 mt-3">Projects</div>
       <p className="mt-3">Here are some stuff I've worked on in the past</p>
-      <div className="grid grid-cols-2 gap-24 mt-9 ml-32  ">
-        <div>
+      <div className="grid grid-cols-2 gap-x-24 mt-9 ml-32">
+        <div className="row-span-2">
           <img
             src={Museum}
             alt="Databases Project for COSC 3380"
@@ -16,13 +16,41 @@ function Projects() {
         </div>
         <div>
           <h1 className="font-bold">The Baker Museum</h1>
-          <p className="mt-6">
-            This was a fun project I enjoyed working on with my friends. In this
-            project, we created a fullstack site using many tools such as Node,
-            mySQL workbench, Postman, Azure, Render, and Vercel.
+          <p className="mt-6 mr-6">
+            This project was an engaging endeavor undertaken with my peers. We
+            developed a comprehensive full-stack website complete with a
+            relational database, catering to the hypothetical needs of a museum.
+            The website featured essential functionalities such as ticket
+            booking, exhibit management, gift shop inventory, checkout, and user
+            profile management. Users were assigned different roles (customer,
+            employee, admin), each with specific permissions tailored to their
+            responsibilities. Tools/Languages we used are listed below.
           </p>
         </div>
-        <div>skills</div>
+        <div className="grid grid-cols-5">
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-20 h-10">
+            React
+          </div>
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-24 h-10">
+            Tailwind
+          </div>
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-24 h-10">
+            Three.js
+          </div>
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-24 h-10">
+            Node.js
+          </div>
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-24 h-10">
+            MySQL
+          </div>
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-16 h-10">JWT</div>
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-20 h-10">
+            Azure
+          </div>
+          <div className="bg-gray-200 rounded-3xl pl-4 pt-2 w-36 h-10">
+            Vercel/Render
+          </div>
+        </div>
       </div>
     </div>
   );
