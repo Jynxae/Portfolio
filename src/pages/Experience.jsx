0bc3a4e30c3284a94590123ad06d1babf389bc89
Reem Alkhalily <realkhalily@hotmail.com>
2024-10-25 17:04:16 -0500

    Added images, linked buttons to respective sites

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,6 +6,27 @@ function Experience() {
       <h2 className="text-[#AD8B73] text-6xl pt-24 pl-24">
         &gt; Work Experience
       </h2>
+      <ul className="text-[#CEAB93] text-4xl pl-24">
+        <li>
+          Software Engineer Intern - NASA <br />
+          <span className="text-[#E3CAA5]">May 2024 - Present</span>
+          <br />
+          I’m assisting the JSL network team to develop a program to streamline
+          <br></br> the process of a network configuration tool to minimize
+          human error using <br></br>SQLite, Python, and React. Additionally, I
+          am currently creating an internal <br></br>application to analyze data
+          given by Wireshark to calculate data using <br></br>Pandas, SQLite,
+          and pyQT.
+        </li>
+        <br></br>
+        <li>
+          Academic Support Assistance - University of Houston<br></br>
+          <span className="text-[#E3CAA5]">January 2024 - Present</span>
+          <br></br>Supported over 240+ students by leading weekly labs teaching
+          the <br></br>fundamentals of C++ programming in addition to providing
+          <br></br> individualized feedback and one-on-one assistance.
+        </li>
+      </ul>
     </div>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,61 @@
 import React from "react";
+import SplashScreen from "../Images/SplashScreen.png";
+import EditBudgetView from "../Images/EditBudgetView.png";
 
 function Projects() {
   return (
     <div className="bg-[#FFFBE9] h-screen w-screen">
       <h2 className="text-[#AD8B73] text-6xl pt-24 pl-24">&gt; Projects</h2>
+      <ul className="text-[#CEAB93] text-4xl pl-24">
+        <li>
+          <span className="text-5xl">PiggyBudgeting</span>
+          <br />
+          Developed a budgeting application that allows users to log
+          transactions, set budgets, and view their spending habits.
+          <div className="flex row">
+            <img src={SplashScreen} className="h-96 w-60 pr-12 mt-3"></img>
+            <img src={EditBudgetView} className="h-96 w-60 pr-12 mt-3"></img>
+          </div>
+          <button
+            onClick={() =>
+              window.open("https://github.com/Jynxae/Team1_BudgetApp", "_blank")
+            }
+            className="mr-5 mt-3 px-4 py-2 bg-[#AD8B73] text-white rounded hover:bg-[#CEAB93] transition duration-300"
+          >
+            View Code
+          </button>
+        </li>
+        <li className="mt-12">
+          <span className="text-5xl">The Baker Museum</span>
+          <br />
+          Led a team of 4 to develop a database system tailored towards an Art
+          Museum.
+        </li>
+        <br />
+        <div>
+          <iframe
+            src="https://www.bakermuseum.art/"
+            title="Baker Museum"
+            className="w-[900px] h-[500px] border-2 border-[#AD8B73] rounded-md"
+          ></iframe>
+          <button
+            onClick={() =>
+              window.open("https://www.bakermuseum.art/", "_blank")
+            }
+            className="mr-5 mt-3 px-4 py-2 bg-[#AD8B73] text-white rounded hover:bg-[#CEAB93] transition duration-300"
+          >
+            View Website
+          </button>
+          <button
+            onClick={() =>
+              window.open("https://github.com/Ashishjob/museuma", "_blank")
+            }
+            className="mr-5 mt-3 px-4 py-2 bg-[#AD8B73] text-white rounded hover:bg-[#CEAB93] transition duration-300"
+          >
+            View Code
+          </button>
+        </div>
+      </ul>
     </div>
   );
 }
